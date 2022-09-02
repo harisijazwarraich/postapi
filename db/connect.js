a9b105598fad22c7cb4d96c8174e885d7fdec93a
Haris <journeyslide@gmail.com>
2022-09-02 07:55:36 -0400

    change

--- db/connect.js
+++ db/connect.js
@@ -2,14 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connectDB = (url) => {
-   return mongoose.connect(url, {
-      server: {
-         socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 }
-      },
-      replset: {
-         socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 }
-      }
-   }
+   return mongoose.connect(url
    );
 }
 
